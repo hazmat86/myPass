2e865fe968bf067b5272f3d62b931590810ebef7
Timothy Chatman <tjchatman1986@gmail.com>
2024-07-28 07:53:07 -0400

    Add pass.h header file

--- src/pass.h
+++ src/pass.h
@@ -1,11 +1,12 @@
 #ifndef PASS_H
 #define PASS_H
 
-
-
 #endif
 
 // Structs that for users, accounts, etc....
 typedef struct User_Acct {
-    
-} user_acct;
+  char* username;
+  char* password;
+  char* email;
+  bool encryption; 
+} user_acct_t;
